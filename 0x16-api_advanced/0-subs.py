018cd39bdccc24bddc47d93673023ca9b79ddc66
randaaz <randaaziz972@gmail.com>
2024-04-09 07:23:23 -0700

    a function that queries the Reddit API and returns the number of subscribers (not active users, total subscribers) for a given subreddit. If an invalid subreddit is given, the function should return 0.

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,45 +1,17 @@
 #!/usr/bin/python3
-""""""
+"""Subscribers on a given Reddit subreddit."""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """
-    Queries the Reddit API and returns the number of subscribers for a given subreddit.
-
-    Args:
-        subreddit: A string representing the name of the subreddit.
-
-    Returns:
-        The number of subscribers (int) for the given subreddit. If the subreddit is invalid, returns 0.
-    """
-    # Construct the URL for the Reddit API endpoint
+    """Queries the Reddit API and returns the number
+    of subscribers for a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    
-    # Define custom headers with a User-Agent to prevent "Too Many Requests" error
     headers = {
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
-    
-    # Send an HTTP GET request to the API endpoint
     response = requests.get(url, headers=headers, allow_redirects=False)
-    
-    # Check if the subreddit is invalid (status code 404)
     if response.status_code == 404:
         return 0
-    
-    # Parse the JSON response and extract the number of subscribers
     results = response.json().get("data")
     return results.get("subscribers")
-
-if __name__ == "__main__":
-    # If script is run directly, retrieve subreddit name from command-line argument
-    import sys
-    
-    # Check if a subreddit name is provided as a command-line argument
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
-    else:
-        # Call number_of_subscribers function with the provided subreddit name
-        print("{:d}".format(number_of_subscribers(sys.argv[1])))
-
