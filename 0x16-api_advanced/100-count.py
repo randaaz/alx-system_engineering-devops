ceda38d0a76a374174d57907c94e7077f2d4eacd
randaaz <randaaziz972@gmail.com>
2024-04-09 07:36:44 -0700

    recursive function that queries the Reddit API, parses the title of all hot articles, and prints a sorted count of given keywords (case-insensitive, delimited by spaces. Javascript should count as javascript, but java should not).

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,16 +1,12 @@
 #!/usr/bin/python3
-""" Module for a function that queries the Reddit API recursively."""
+""" Count occurrences of words in hot articles of a subreddit."""
 
 
 import requests
 
 
 def count_words(subreddit, word_list, after='', word_dict={}):
-    """ A function that queries the Reddit API parses the title of
-    all hot articles, and prints a sorted count of given keywords
-    (case-insensitive, delimited by spaces.
-    Javascript should count as javascript, but java should not).
-    If no posts match or the subreddit is invalid, it prints nothing.
+    """Count occurrences of words in hot articles of a subreddit.
     """
 
     if not word_dict:
